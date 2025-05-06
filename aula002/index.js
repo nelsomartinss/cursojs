c65d61dc6ec44cd735c3340ffe659ff802192854
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-05-06 12:24:42 -0300

    aprendi comentarios em javascript

---
+++ aula002/index.js
@@ -0,0 +1,16 @@
+// Comentários
+
+// Comentários de uma linha
+
+/*
+    Comentarios
+    de 
+    mais
+    de 
+    uma
+    linha (comentar io de bloco)
+*/
+
+// Comentários são trechos de código que são ignorados pelo motor/engine do javascript (o node usa o v8 - o mesmo motor do chrome)
+console.log('essa é a linha 15, as linhas de comentários são ignoradas');
+// console.log('esse trecho de código eu não desejo executar') - Podemos usar os comentários para desabilitar alguma linha de código
