0211ad2cd93ba7190cf7030cd327b8ca6761e5be
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-05-06 12:41:01 -0300

    aprendi as diferenças de executar o código no node ou no navegador linkando a um arquivo html

---
+++ aula003/index.js
@@ -0,0 +1,13 @@
+console.log('Podemos executar os códigos com node nomedoarquivo.js')
+
+// usaremos o terminal para usar o comando node index.js para executar esse arquivo
+// usamos cd nomedapasta para acessar a pasta que estamos utilizando
+// usamos clear ou cls para limpar o terminal (dependendo do terminal)
+
+console.log('esse texto será exibindo no console do navegador.')
+
+// é interessate sempre usar um arquivo a parte para usar o javascript
+
+alert('Esse é um comando que só vai funcionar no navegador, no node não vai funcionar!'); // o comando alert exibe uma caixinha no navegador que vai travar o carregamento do site até o usuário clicar em 'enter'
+
+// se tentarmos executar o alert() no node ele vai resultar em alert is not defined, no navegador vai funcionar
