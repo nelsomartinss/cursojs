55f880df1095996629e3f35546ce43ae4c57756c
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-06-02 22:18:57 -0300

    aprendi sobre operações ternárias

---
+++ aula022/index.js
@@ -0,0 +1,9 @@
+// Operação ternária ? : 
+
+// if = ? 
+// else = :
+
+// Exemplo pratico 
+const nota = 10; 
+const resultado = nota <= 6 ? 'Reprovado' : 'Aprovado'; // verificando nota
+console.log(resultado); // exibindo 
